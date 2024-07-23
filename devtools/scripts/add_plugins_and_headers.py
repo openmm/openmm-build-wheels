65fe94b161c770cf209f94190dbd0eb441be137b
peastman <peastman@stanford.edu>
2024-07-22 17:10:34 -0700

    Debugging

--- devtools/scripts/add_plugins_and_headers.py
+++ devtools/scripts/add_plugins_and_headers.py
@@ -5,7 +5,9 @@ import shutil
 import sys
 
 install_dir = sys.argv[1]
+print('current dir:', os.getcwd())
 for filename in os.listdir('fixed'):
+    print('processing', filename)
     with wheeltools.InWheel(filename, filename):
         shutil.copytree(join(install_dir, 'lib', 'plugins'), 'OpenMM.libs/lib/plugins')
         shutil.copytree(join(install_dir, 'include'), 'OpenMM.libs/include')
