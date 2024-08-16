e602331255bdf1910b79094f7beaf5f8e77b4da0
peastman <peastman@stanford.edu>
2024-08-16 14:05:23 -0700

    Debugging

--- devtools/scripts/add_plugins_and_headers.py
+++ devtools/scripts/add_plugins_and_headers.py
@@ -8,5 +8,5 @@ install_dir = sys.argv[1]
 for filename in os.listdir('fixed'):
     path = os.path.join('fixed', filename)
     with wheeltools.InWheel(path, path):
-        shutil.copytree(join(install_dir, 'lib'), 'OpenMM.libs/lib')
-        shutil.copytree(join(install_dir, 'include'), 'OpenMM.libs/include')
+        shutil.copytree(join(install_dir, 'lib'), 'OpenMM.libs/lib', dirs_exist_ok=True)
+        shutil.copytree(join(install_dir, 'include'), 'OpenMM.libs/include', dirs_exist_ok=True)
