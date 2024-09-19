e704e6cf876d0eb9f39f901260e5b877346bbcf7
peastman <peastman@stanford.edu>
2024-09-19 08:51:58 -0700

    Omit CUDA from wheel

--- devtools/scripts/add_plugins_and_headers.py
+++ devtools/scripts/add_plugins_and_headers.py
@@ -5,8 +5,18 @@ import shutil
 import sys
 
 install_dir = sys.argv[1]
+if len(sys.argv) == 2:
+    ignore = None
+elif sys.argv[2][0] == '!':
+    platform = sys.argv[2][1:]
+    def ignore(dir, files):
+        return [f for f in files if platform in f]
+else:
+    platform = sys.argv[2]
+    def ignore(dir, files):
+        return [f for f in files if platform not in f]
 for filename in os.listdir('.'):
     if filename.endswith('.whl'):
         with wheeltools.InWheel(filename, filename):
-            shutil.copytree(join(install_dir, 'lib'), 'OpenMM.libs/lib', dirs_exist_ok=True)
+            shutil.copytree(join(install_dir, 'lib'), 'OpenMM.libs/lib', dirs_exist_ok=True, ignore=ignore)
             shutil.copytree(join(install_dir, 'include'), 'OpenMM.libs/include', dirs_exist_ok=True)
