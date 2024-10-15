5d962f9944a833361ca049dd1591f08a36ff54e4
Peter Eastman <peastman@stanford.edu>
2024-10-15 12:04:13 -0700

    Build separate wheel for HIP (#7)
    * Build separate wheel for HIP
    
    * Fixed error
    
    * Build for Windows
    
    * Debugging
    
    * Debugging
    
    * Removed debugging code

--- devtools/scripts/add_plugins_and_headers.py
+++ devtools/scripts/add_plugins_and_headers.py
@@ -7,14 +7,15 @@ import sys
 install_dir = sys.argv[1]
 if len(sys.argv) == 2:
     ignore = None
-elif sys.argv[2][0] == '!':
-    platform = sys.argv[2][1:]
-    def ignore(dir, files):
-        return [f for f in files if platform in f]
 else:
-    platform = sys.argv[2]
+    platforms = sys.argv[2:]
     def ignore(dir, files):
-        return [f for f in files if platform not in f and not isdir(join(dir, f))]
+        for platform in platforms:
+            if platform[0] == '!':
+                files = [f for f in files if platform[1:] in f]
+            else:
+                files = [f for f in files if platform not in f and not isdir(join(dir, f))]
+        return files
 for filename in os.listdir('.'):
     if filename.endswith('.whl'):
         with wheeltools.InWheel(filename, filename):
