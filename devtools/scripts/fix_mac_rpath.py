f01ca14a593599166250b5be27b2abb9cc51c283
peastman <peastman@stanford.edu>
2024-10-18 12:24:32 -0700

    Remove debugging code

--- devtools/scripts/fix_mac_rpath.py
+++ devtools/scripts/fix_mac_rpath.py
@@ -6,12 +6,8 @@ import sys
 install_dir = sys.argv[1]
 for wheelname in os.listdir(install_dir):
     if wheelname.endswith('.whl'):
-        print('wheel name', wheelname)
         with wheeltools.InWheel(wheelname, wheelname):
             for libname in os.listdir('openmm'):
                 if libname.endswith('.so'):
-                    print('lib name', libname)
-                    print('current rpaths:', tools.get_rpaths(join('openmm', libname)))
                     tools._delete_rpaths(join('openmm', libname), ['/Users/runner/openmm-install/lib'])
                     tools.add_rpath(join('openmm', libname), '@loader_path/../OpenMM.libs/lib')
-                    print('new rpaths:', tools.get_rpaths(join('openmm', libname)))
