32130e45b7906740fb022440e649826e55a9dc97
Peter Eastman <peastman@stanford.edu>
2024-10-18 12:49:06 -0700

    Fix incorrect RPATH on Mac (#12)
    * Fix incorrect RPATH on Mac
    
    * Debugging
    
    * Debugging
    
    * Try changing rpath before running delocate
    
    * Try prefixing @loader_path
    
    * Remove debugging code

---
+++ devtools/scripts/fix_mac_rpath.py
@@ -0,0 +1,13 @@
+from delocate import wheeltools, tools
+from os.path import join
+import os
+import sys
+
+install_dir = sys.argv[1]
+for wheelname in os.listdir(install_dir):
+    if wheelname.endswith('.whl'):
+        with wheeltools.InWheel(wheelname, wheelname):
+            for libname in os.listdir('openmm'):
+                if libname.endswith('.so'):
+                    tools._delete_rpaths(join('openmm', libname), ['/Users/runner/openmm-install/lib'])
+                    tools.add_rpath(join('openmm', libname), '@loader_path/../OpenMM.libs/lib')
