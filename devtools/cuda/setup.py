25a347e9dc08b15acc2a5a604d84abc18fa88a10
peastman <peastman@stanford.edu>
2025-11-06 08:37:46 -0800

    Fix package names for CUDA 13

--- devtools/cuda/setup.py
+++ devtools/cuda/setup.py
@@ -3,7 +3,10 @@ import os
 
 OPENMM_VERSION=os.getenv('OPENMM_VERSION')
 CUDA_VERSION=os.getenv('CUDA_VERSION')
-
+if CUDA_VERSION == '12':
+    CUDA_SUFFIX = '-cu12'
+else:
+    CUDA_SUFFIX = '>=13,<14'
 setup(
     name=f'OpenMM-CUDA-{CUDA_VERSION}',
     version=OPENMM_VERSION,
@@ -11,10 +14,10 @@ setup(
     author='Peter Eastman',
     url='https://openmm.org',
     packages=[],
-    install_requires=[f'nvidia-cuda-runtime-cu{CUDA_VERSION}',
-                      f'nvidia-cuda-nvcc-cu{CUDA_VERSION}',
-                      f'nvidia-cuda-nvrtc-cu{CUDA_VERSION}',
-                      f'nvidia-cuda-cupti-cu{CUDA_VERSION}',
-                      f'nvidia-cufft-cu{CUDA_VERSION}'],
+    install_requires=[f'nvidia-cuda-runtime{CUDA_SUFFIX}',
+                      f'nvidia-cuda-nvcc{CUDA_SUFFIX}',
+                      f'nvidia-cuda-nvrtc{CUDA_SUFFIX}',
+                      f'nvidia-cuda-cupti{CUDA_SUFFIX}',
+                      f'nvidia-cufft{CUDA_SUFFIX}'],
     dependency_links=['https://pypi.ngc.nvidia.com']
 )
